e7833b2710856c9845986982037ca0b06a943068
Aries Dmello <dmelloaries@gmail.com>
2025-11-09 01:15:41 +0530

    feat: Set up Docker for client application with Nginx
    - Added Dockerfile to build and serve the client application using Nginx.
    - Created .dockerignore to exclude unnecessary files from the Docker context.
    - Configured Nginx with gzip compression, caching for static assets, and React Router support.
    - Updated CORS configuration in app.js to allow additional origins.

--- server/app.js
+++ server/app.js
@@ -18,7 +18,9 @@ app.use(
     origin:
       process.env.CLIENT_URL ||
       "http://localhost:5173" ||
-      "http://127.0.0.1:5500",
+      "http://127.0.0.1:5500" ||
+      "http://localhost:3000",
+
     credentials: true,
   })
 );
