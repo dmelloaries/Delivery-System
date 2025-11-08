2c4fa03d5b2640c61a73db2220d39e3339bb5e0a
Aries Dmello <dmelloaries@gmail.com>
2025-11-09 01:51:15 +0530

    feat: Add Docker configuration for MongoDB, server, and client with health checks

--- server/app.js
+++ server/app.js
@@ -15,12 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin:
-      process.env.CLIENT_URL ||
-      "http://localhost:5173" ||
-      "http://127.0.0.1:5500" ||
-      "http://localhost:3000",
-
+    origin: true,
     credentials: true,
   })
 );
